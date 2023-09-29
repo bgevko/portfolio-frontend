40ba2d295032020a0113e15abc8130dba1b38626
bgevko <bgevko@gmail.com>
2023-09-29 23:47:40 +0300

    Migrate to vite

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 
 const getUrl = (() => {
-  if (process.env.REACT_APP_ENV === 'dev') {
-    return process.env.REACT_APP_DEV_URL
-  } else if (process.env.REACT_APP_ENV === 'prod') {
-    return process.env.REACT_APP_PROD_URL
+  if (import.meta.env.VITE_ENV === 'dev') {
+    return import.meta.env.VITE_DEV_URL
+  } else if (import.meta.env.VITE_ENV === 'prod') {
+    return import.meta.env.VITE_PROD_URL
   } else {
-    throw new Error('Invalid REACT_APP_ENV in .env file. Set to "dev" or "prod"')
+    throw new Error('Invalid VITE_ENV in .env file. Set to "dev" or "prod"')
   }
 })
 
 const baseUrl = getUrl()
 
 if (!baseUrl) {
-  throw new Error('Invalid .env configuration. Set the REACT_APP_DEV_URL and REACT_APP_PROD_URL variables in the .env file set the REACT_APP_ENV variable to "dev" or "prod"')
+  throw new Error('Invalid .env configuration. Set the VITE_DEV_URL and VITE_PROD_URL variables in the .env file set the VITE_ENV variable to "dev" or "prod"')
 }
 
 export { baseUrl }
